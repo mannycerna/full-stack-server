110e415a9823b59e9187bcafa8a1b2a98cd16857
mannycerna <manuel.cerna@codeimmersives.com>
2023-03-13 13:39:01 -0500

    updated app.js *.json

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-// app.js
 
+//app.js
+//
 const express = require('express');
 
+require('dotenv').config();
+
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));app.js
+//
+//
+//
+//
